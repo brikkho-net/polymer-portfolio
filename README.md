b3a30f5177256194791b77818342cdc6292f0963
Samarth Gulati <samarth@Samarths-MacBook-Air.local>
2015-10-12 01:03:15 -0400

    First pages commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Polymer Portfolio CMS
+
+> A starting point for building a Portfolio with Polymer 1.0
+
+
---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+/*
+Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
+(function (document) {
+  'use strict';
+
+})(document);
+
+// TODO: Decide if we still want to suggest wrapping as it requires
+// using webcomponents.min.js.
+// wrap document so it plays nice with other libraries
+// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
+// )(wrap(document));
